23c4a8eae209d9ffce8a810fb16bd2588cb5aa83
Riazul Hasan <riazul701@gmail.com>
2019-04-07 14:02:32 +0600

    Initial Commit.

---
+++ strings/access_strings/PrintSecondCharacter.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string myString = "Hello";
+  cout << myString[1];
+  return 0;
+}
---
+++ variables/ConstantExample.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  const int minutesPerHour = 60;
+  const float PI = 3.14;
+  cout << minutesPerHour << "\n";
+  cout << PI;
+  return 0;
+}
